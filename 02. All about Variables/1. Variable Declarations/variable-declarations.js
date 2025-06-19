374de8f0098bcd416477f03c3e247508ee81e595
Kirin <noctulvia@outlook.com>
2025-06-20 01:06:31 +0800

    Enhance README.md and variable-declarations.js with clearer explanations of var, let, and const usage, including details on scope and reassignment.

--- 02. All about Variables/1. Variable Declarations/variable-declarations.js
+++ 02. All about Variables/1. Variable Declarations/variable-declarations.js
@@ -1,11 +1,17 @@
-// 1. var
+// 1. 使用 var 声明变量
 var a = 10;
 console.log(a); // 10
+// 重新赋值使用 var 声明的变量
+var a = 20;
+console.log(a); // 20
 
-// 2. let
+// 2. 使用 let 声明变量
 let b = 20
 console.log(b); // 20
+// 重新赋值使用 let 声明的变量
+b = 30;
+console.log(b); // 30
 
-// 3. const
+// 3. 使用 const 声明常量
 const z = 30;
 console.log(z); // 30
